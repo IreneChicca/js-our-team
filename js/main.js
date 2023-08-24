540e41080a48021f0b2309da247094201fd4d69f
IreneChicca <irene.chicca@outlook.com>
2023-08-24 14:44:04 +0200

    Milestone 1

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const team = [
+
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        photo: 'scott-estrada-developer.jpg',
+    },
+
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg',
+    }
+
+
+]
+
+
